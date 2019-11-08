3da82801f655b9e0c8f5596fa525d116dbf1f71f
Ridvan Cekmez <ridvanceko@gmail.com>
2019-11-07 19:35:12 -0700

    Connected with routers

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "burger_db"
+})
+
+connection.connect(function(err){
+  if(err)throw err;
+  console.log("Connected:" + connection.threadId);
+
+})
+
+module.exports = connection;
