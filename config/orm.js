1019990826fabd35935aa0921104ad279eadd0b5
Ridvan Cekmez <ridvanceko@gmail.com>
2019-11-13 14:48:54 -0700

    create method added

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,28 @@
 var connection = require("./connection.js");
 
 var orm = {
-  all: function(tableInput, cb){
-    connection.query("SELECT * FROM " + tableInput + ";" , function(err, result){
-      if(err) throw err;
+  all: function (tableInput, cb) {
+    connection.query("SELECT * FROM " + tableInput + ";", function (err, result) {
+      if (err) throw err;
       cb(result)
     })
   },
 
-update: function(tableInput, condition, cb){
-  connection.query("UPDATE "+tableInput+" SET devoured=true WHERE id="+condition+";", function(err,result){
-    
-  })
-}
+  update: function (tableInput, condition, cb) {
+    connection.query("UPDATE " + tableInput + " SET devoured=true WHERE id=" + condition + ";", function (err, result) {
 
+
+
+    })
+  },
+
+  create: function (tableInput, val, cb) {
+    connection.query('INSERT INTO ' + tableInput + " (burger_name) VALUES ('"+val+"');", function (err, result) {
+      if (err) throw err;
+      cb(result);
+    })
+
+  }
 }
 
 module.exports = orm;
--- models/burger.js
+++ models/burger.js
@@ -8,6 +8,10 @@ var burger = {
   },
 update: function(id,cb){
   orm.update("burgers",id,cb);
+},
+
+create: function(name,cb){
+  orm.create("burgers", name, cb);
 }
 
 }
