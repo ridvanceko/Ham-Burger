fbe90ba7cc4155a8c68134ee3274664259acad19
Ridvan Cekmez <ridvanceko@gmail.com>
2019-11-21 12:18:43 -0700

    handlebars displayed at browser

--- controllers/routes.js
+++ controllers/routes.js
@@ -3,18 +3,13 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
-router.get("/", function(req,res){
-  burger.all(function(burger_data){
-    console.log(burger_data);
-    res.render("index", {burger_data});
-  })
- 
-})
 
-router.put("/burgers/update", function(req,res){
+router.post("/burgers/update", function(req,res){
+  // console.log(req.params.burger_id);
+
   burger.update(req.body.burger_id, function(result){
     console.log(result);
-    res.redirect("/");
+    res.redirect("back");
   });
 });
 
@@ -22,6 +17,14 @@ router.post('/burgers/create', function(req,res){
   burger.create(req.body.burger_name, function(result){
     res.redirect('/');
   })
+})
+
+router.get("/", function(req,res){
+  burger.all(function(burger_data){
+    console.log(burger_data);
+    res.render("index", {burger_data});
+  })
+ 
 })
 
 module.exports = router;
