216375439192d8e26fd1808e946a57f5c5d1b3f6
Ridvan Cekmez <ridvanceko@gmail.com>
2019-12-05 15:32:26 -0700

    jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ var connection;
   if(process.env.JAWSDB_AMBER_URL) {connection = mysql.createConnection(process.env.JAWSDB_AMBER_URL);
   } else{
     connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "burger_db"
+  host: "hcm4e9frmbwfez47.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "fcn4qjc9lrzi6gvd",
+  password: "	icejn4mrnmkey81i",
+  database: "	v1f3fy01s7bplfxi"
   });
   };
 
